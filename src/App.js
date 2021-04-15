472ac4a348ba2bcd3bba0318af5768b21d718d5b
andrewsilent <silent.netix@gmail.com>
2021-04-15 23:26:10 +0300

    add basic structures

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Todo = lazy(() => import('./pages/Todo.jsx'));
+
 const App = props => {
   return (
-    <section>
-      <h1>Hello World</h1>
-    </section>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/tasks'>Todo</Link>
+          </li>
+        </ul>
+      </nav>
+      <Suspense fallback={'Loading...'}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/tasks' component={Todo} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
 };
 
